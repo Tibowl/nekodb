e360484cad5d0c8abae9f26e31b58bb7c1ad14fa
Tibowl <Tibowl@users.noreply.github.com>
2024-11-19 23:04:33 +0100

    Add animations for myneko and helper

--- pages/cats/[catId].tsx
+++ pages/cats/[catId].tsx
@@ -116,7 +116,18 @@ export const getStaticProps = (async (context) => {
       const xmlPath = `/na2-assets/neko/normal/master_xml/${image.replace(".png", ".xml")}`
       return await getAnimation(image, imagePath, xmlPath)
     }))).filter(x => x != null))
-  } else if (type == CatType.Rare) {
+  } else if (type == CatType.Myneko) {
+      const myNekoCats = await readdir("public/na2-assets/neko/myneko")
+      const match = myNekoCats.find(f => f.startsWith(cat.Id.toString().slice(1) + "_"))
+      if (!match) throw new Error("No animations found for " + smallCat.name)
+      const images = await readdir(`public/na2-assets/neko/myneko/${match}`)
+  
+      animations.push(...(await Promise.all(images.map(async image => {
+        const imagePath = `/na2-assets/neko/myneko/${match}/${image}`
+        const xmlPath = `/na2-assets/neko/normal/master_xml/${image.replace(".png", ".xml")}`
+        return await getAnimation(image, imagePath, xmlPath)
+      }))).filter(x => x != null))
+  } else if (type == CatType.Rare || type == CatType.Other) {
     const rareCats = await readdir("public/na2-assets/neko/special/png")
     const match = rareCats.filter(f => f.startsWith(getCatIconId({ id: cat.Id }).replace("s", "sp") + "_"))
     if (match.length == 0) throw new Error("No animations found for " + smallCat.name)
