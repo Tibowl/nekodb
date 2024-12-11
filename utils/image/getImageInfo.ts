71122dafe4910745aecef960db7ce690575365ee
Tibowl <Tibowl@users.noreply.github.com>
2024-12-11 06:45:32 +0100

    Don't fail if image not found

--- utils/image/getImageInfo.ts
+++ utils/image/getImageInfo.ts
@@ -6,14 +6,23 @@ export default async function getImageInfo(image: string) {
         throw new Error("Invalid image path for " + image)
     }
 
-    const imageDimensions = await getImageSize(await readFile(`public/${image}`))
-    if (imageDimensions.width == undefined || imageDimensions.height == undefined) {
-        throw new Error("Invalid image dimensions for " + image)
-    }
-
-    return {
-        url: image,
-        width: imageDimensions.width,
-        height: imageDimensions.height
+    try {
+        const imageDimensions = await getImageSize(await readFile(`public/${image}`))
+        if (imageDimensions.width == undefined || imageDimensions.height == undefined) {
+            throw new Error("Invalid image dimensions for " + image)
+        }
+    
+        return {
+            url: image,
+            width: imageDimensions.width,
+            height: imageDimensions.height
+        }
+    } catch (error) {
+        console.error(`Failed to get image info for ${image}: ${error}`)
+        return {
+            url: image,
+            width: 0,
+            height: 0
+        }
     }
 }
