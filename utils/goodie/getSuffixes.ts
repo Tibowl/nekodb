454b0233bc6d63b886e0533188dacb80810be618
Tibowl <Tibowl@users.noreply.github.com>
2026-04-01 21:49:32 +0200

    Add custom suffixes for repair pattern -2

--- utils/goodie/getSuffixes.ts
+++ utils/goodie/getSuffixes.ts
@@ -1,7 +1,7 @@
 import { GoodieRecord } from "../tables"
 
 export function getSuffixes(goodie: GoodieRecord) {
-  if (goodie.Id == 320) return [
+  if (goodie.Id == 320) return [ // Bunny Bed DX
     // This goodie has RepairPattern 2 but has a shared repair_break
     "",
     "_break",
@@ -9,6 +9,21 @@ export function getSuffixes(goodie: GoodieRecord) {
     "_repair_2",
     "_repair_break",
   ]
+
+  if (goodie.RepairPattern == -2) {
+    // This goodie has a unique repair pattern that doesn't follow the normal rules, so we just hardcode the suffixes
+    if (goodie.Id == 332) return [ // Shoji Screen Stand
+      "",
+      "_break",
+    ]
+
+    if (goodie.Id == 337) return [ // Instant Camera
+      "",
+      "_break",
+      "_repair_break",
+    ]
+    throw new Error(`Goodie with id ${goodie.Id} has RepairPattern -2 but is not handled in getSuffixes`)
+  }
 
   if (goodie.Toughness == 0) return [""]
   if (goodie.RepairPattern == 0) return [
